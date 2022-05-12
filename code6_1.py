c0f00fb1c00e5ccc8b992477a9c14c7fc79394b7
dotdotot <dotdotot203@naver.com>
2022-05-12 21:52:45 +0900

    수정사항

--- code6_1.py
+++ code6_1.py
@@ -11,7 +11,7 @@ val = add(2,3)
 print("{} + {} = {}".format(2,3,val))
 
 # val 변수 선언 및 초기화
-# 초기화는 add함수를 호출하여 return 값으로 초기화한다 (인수로 2,3을 넘긴다)
-val = add(2,3)
+# 초기화는 add함수를 호출하여 return 값으로 초기화한다 (인수로 7,8을 넘긴다)
+val = add(7,8)
 # print에 format을 지정하여 출력
 print("{} + {} = {}".format(2,3,val))
---
+++ code6_10.py
@@ -0,0 +1,11 @@
+# def 함수 선언 및 정의 
+def rectArea(w,h):
+    # area 변수 선언 및 w * h로 초기화
+    area = w*h
+    return area
+
+# area 변수 선언 및 rectArea함수에 3을 인수로 넘겨 초기화
+area = rectArea(3)
+
+# rectArea 함수에는 매개 변수가 2개이므로 인수를 1개만 넘긴다면 에러가 발생함
+
